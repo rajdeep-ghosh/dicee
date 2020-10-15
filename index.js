64f4318d9545d3b28a70f5a11f6d6c599d61e8e4
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2020-10-16 04:36:22 +0530

    Create Roll Dice button

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var randomVariable1 = Math.floor(Math.random() * 6) + 1;
-var randomVariable2 = Math.floor(Math.random() * 6) + 1;
-var result;
+document.querySelector(".btn").addEventListener("click", function() {
+    var randomVariable1 = Math.floor(Math.random() * 6) + 1;
+    var randomVariable2 = Math.floor(Math.random() * 6) + 1;
+    var result;
 
-if(randomVariable1 > randomVariable2) {
-    result = "🥇Player 1 Wins!";
-}
-else if(randomVariable1 < randomVariable2) {
-    result = "Player 2 Wins!🥇";
-}
-else {
-    result = "Draw!";
-}
+    if(randomVariable1 > randomVariable2) {
+        result = "🥇Player 1 Wins!";
+    }
+    else if(randomVariable1 < randomVariable2) {
+        result = "Player 2 Wins!🥇";
+    }
+    else {
+        result = "Draw!";
+    }
 
-document.querySelector("h1").textContent = result;
+    document.querySelector("h1").textContent = result;
 
-var randomImageSource1 = "images/dice" + randomVariable1 + ".png";
-var randomImageSource2 = "images/dice" + randomVariable2 + ".png";
+    var randomImageSource1 = "images/dice" + randomVariable1 + ".png";
+    var randomImageSource2 = "images/dice" + randomVariable2 + ".png";
 
-document.querySelector(".img1").setAttribute("src", randomImageSource1);
-document.querySelector(".img2").setAttribute("src", randomImageSource2);
+    document.querySelector(".img1").setAttribute("src", randomImageSource1);
+    document.querySelector(".img2").setAttribute("src", randomImageSource2);
+});
